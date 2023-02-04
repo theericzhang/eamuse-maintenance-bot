e5bed0f30f9bc9a2cc63eb69a3065aed841a4970
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-02-03 20:27:10 -0500

    detect if ics has been written already, if so, send path to that file

--- helper-functions/ics-generator/ics-generator.js
+++ helper-functions/ics-generator/ics-generator.js
@@ -1,40 +1,42 @@
 import ics from 'ics';
-import { writeFileSync } from 'fs';
+import { writeFileSync, existsSync } from 'fs';
 
 /**
  *
  * @param {Object<Date>} nextMaintenanceDate - The object containing a maintenance date start date, start time, and end time.
- * @returns {File} - An ICS File that will be streamed to the channel.
+ * @returns {string} - An ICS Filepath that will be streamed to the channel.
  */
 export default function generateICS(nextMaintenanceDate) {
     const icsCreator = ics;
     const dateStart = nextMaintenanceDate.dateStartUTC;
-    console.log(dateStart);
     const dateEnd = nextMaintenanceDate.dateEndUTC;
-    // console.log(dateStart.getUTCMonth(), dateStart.getUTCDay());
-    // console.log(dateStartYear, dateStartMonth, dateStartDay, dateStartHour, dateStartMinute);
-    icsCreator.createEvent({
-        title: 'e-amusement Extended Maintenance',
-        description: 'e-amusement Service will be down during this time',
-        start: [
-            dateStart.getUTCFullYear(),
-            dateStart.getUTCMonth() + 1,
-            dateStart.getUTCDate(),
-            dateStart.getUTCHours(),
-            dateStart.getUTCMinutes(),
-        ],
-        startInputType: 'utc',
-        end: [
-            dateEnd.getUTCFullYear(),
-            dateEnd.getUTCMonth() + 1,
-            dateEnd.getUTCDate(),
-            dateEnd.getUTCHours(),
-            dateEnd.getUTCMinutes(),
-        ],
-    }, (error, value) => {
-        if (error) {
-            console.log(error);
-        }
-        writeFileSync(`${process.cwd()}/event.ics`, value);
-    });
+
+    if (!existsSync(`${process.cwd()}/event_${dateStart}.ics`)) {
+        icsCreator.createEvent({
+            title: 'e-amusement Extended Maintenance',
+            description: 'e-amusement Service will be down during this time',
+            start: [
+                dateStart.getUTCFullYear(),
+                dateStart.getUTCMonth() + 1,
+                dateStart.getUTCDate(),
+                dateStart.getUTCHours(),
+                dateStart.getUTCMinutes(),
+            ],
+            startInputType: 'utc',
+            end: [
+                dateEnd.getUTCFullYear(),
+                dateEnd.getUTCMonth() + 1,
+                dateEnd.getUTCDate(),
+                dateEnd.getUTCHours(),
+                dateEnd.getUTCMinutes(),
+            ],
+        }, (error, value) => {
+            if (error) {
+                console.log(error);
+            }
+            writeFileSync(`${process.cwd()}/event_${dateStart}.ics`, value);
+        });
+    } else {
+        console.log('already made file, returning existing file');
+    }
 }
